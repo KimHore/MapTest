31cef04b9acbff002a8a332aa2d8c06275a55eb4
Chawakorn Jewpatthanakul <innocentjunk@gmail.com>
2016-04-08 00:49:02 +0700

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by DE DPU on 4/8/2559 BE.
+//  Copyright © 2559 Kimhore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
